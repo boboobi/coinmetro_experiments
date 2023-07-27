47ebb6e5e654fcd52355264588446b63385b58fb
the beast <140100388+the-monster-beast@users.noreply.github.com>
2023-07-27 18:42:27 +0200

    Update coinmetro_bot.py

--- coinmetro_bot.py
+++ coinmetro_bot.py
@@ -1,12 +1,18 @@
 import json
 import os
 import requests
+import time
 
+from command import Command, match_command
+
+LAG = 60  # seconds to wait before refreshing cache
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 COINMETRO_ENDPOINT = os.environ.get('COINMETRO_ENDPOINT')
 PRICES_ENDPOINT = "/exchange/prices"
 NOMINATING_ASSETS = ['USD', 'EUR', 'GBP', 'BTC', 'ETH', 'AUD']
+
+response_cache = {}
 
 
 def lambda_handler(event, _):
@@ -32,37 +38,56 @@ def lambda_handler(event, _):
 
 
 def generate_text_response(message_part):
-    if message_part.startswith('/volume'):
-        total_volume, volumes = get_volume()
-        nb_top = 3
-        top = format_top_volumes(volumes, nb_top)
-        return f"The current 24h volume on Coinmetro is ${total_volume:,.2f}" \
-            f"\n\n Top {nb_top}: {top}"
-    elif message_part.startswith('/admin'):
-        return "@xcmonika @xcmusab @herebycm @reddug @XCMkellyXCM @JensAtDenmark @medatank @WillDec"
-    elif message_part.startswith('/start'):
+    response = None
+    command = match_command(message_part)
+    if command is Command.START:
         return "Hey, I'm an unofficial bot for Coinmetro. Use the /help " \
                "command to see an overview of currently available commands."
-    elif message_part.startswith('/help'):
-        return "Here's an overview of commands : \n/admin : ping admins \n/volume : get 24h volume"
-    return None
+    elif command is Command.HELP:
+        return "Here's an overview of commands : \n" \
+               "/admin : ping admins \n" \
+               "/volume : get 24h volume"
+    elif command is Command.VOLUME:
+        return get_with_caching(command, get_volume)
+    elif command is Command.ADMIN:
+        return "@xcmonika @xcmusab @herebycm @reddug @XCMkellyXCM " \
+               "@JensAtDenmark @medatank @WillDec"
+    return response
 
 
-def get_volume():
+def update_cache(command, response):
+    response_cache.update({command: (time.time(), response)})
+
+
+def get_with_caching(command, function):
+    if command in response_cache:
+        tic, response = response_cache.get(command)
+        if time.time() - tic < LAG:
+            return response
+    response = function()
+    update_cache(command, response)
+    return response
+
+
+def get_volume(nb_top=3):
     response = requests.get(f"{COINMETRO_ENDPOINT}{PRICES_ENDPOINT}")
     if response.status_code == 200:
         response_json = response.json()
-        return calculate_volumes(response_json)
+        total_volume, volumes = calculate_volumes(response_json)
+        top = format_top_volumes(volumes, nb_top)
+        return f"The current 24h volume on Coinmetro is " \
+                   f"${total_volume:,.2f}\n\n " \
+                   f"Top {nb_top}: {top}"
     else:
         print("API call failed.")
     return None
-    
+
 
 def format_top_volumes(volumes, nb_top=3):
-    sorted_volumes = sorted(volumes.items(), key=lambda x:x[1], reverse=True)
+    sorted_volumes = sorted(volumes.items(), key=lambda x: x[1], reverse=True)
     top = '\n\t'.join([format_volume(sorted_volumes[i]) for i in range(3)])
     return f"\n\t{top}"
-    
+
 
 def format_volume(tuple):
     return f"${tuple[0]}: ${tuple[1]:,.2f}"
